8f18e01d628ce08627b3f3d29ae1d0737b6a7b20
sin-ami <amikojima12@gmail.com>
2026-05-12 13:03:14 +0900

    Initial Commit

---
+++ Shake/ShakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeApp.swift
+//  Shake
+//
+//  Created by 小島愛実 on 2026/05/12.
+//
+
+import SwiftUI
+
+@main
+struct ShakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
